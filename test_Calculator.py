e1fb5b742d082f6db4db029b62f8b1a0522ffe57
po47 <po47@njit.edu>
2020-01-28 14:53:53 -0500

    Initial commit

---
+++ test_Calculator.py
@@ -0,0 +1,9 @@
+import unittest
+
+class MyTestCase(unittest.TestCase):
+
+    def test_instantiate_calculator(self):
+        self.assertIsInstance(self.calculator, Calculator)
+
+if __name__ == '__main__':
+    unittest.main()
